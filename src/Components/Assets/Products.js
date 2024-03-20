b96ece80ffe0970f131a6314a6331132f972cb11
acer <acer@PRO500AM4-M5>
2024-03-20 17:51:41 +0530

    assets

---
+++ src/Components/Assets/Products.js
@@ -0,0 +1,35 @@
+import P1 from './P1.png'
+
+let products = [
+    {
+        id : 1,
+        name : "Sony WH-1000XM4 Industry Leading Wireless Noise Cancellation Bluetooth Over Ear Headphones with Mic for Phone Calls, 30 Hours Battery Life, Quick Charge, AUX, Touch Control and Voice Control - BlacK...",
+        price : 1200,
+        image : P1,
+        rating : 4.5
+    }, {
+        id : 2,
+        name : "Sony WH-1000XM4 Industry Leading Wireless Noise Cancellation Bluetooth Over Ear Headphones with Mic for Phone Calls, 30 Hours Battery Life, Quick Charge, AUX, Touch Control and Voice Control - BlacK...",
+        price : 1250,
+        image : P1,
+        rating : 4.0
+    },{
+        id : 3,
+        name : "Sony WH-1000XM4 Industry Leading Wireless Noise Cancellation Bluetooth Over Ear Headphones with Mic for Phone Calls, 30 Hours Battery Life, Quick Charge, AUX, Touch Control and Voice Control - BlacK...",
+        price : 1300,
+        image : P1,
+        rating : 4.6
+    },{
+        id : 4,
+        name : "Sony WH-1000XM4 Industry Leading Wireless Noise Cancellation Bluetooth Over Ear Headphones with Mic for Phone Calls, 30 Hours Battery Life, Quick Charge, AUX, Touch Control and Voice Control - BlacK...",
+        price : 1600,
+        image : P1,
+        rating : 4.3
+    },{
+        id : 5,
+        name : "Sony WH-1000XM4 Industry Leading Wireless Noise Cancellation Bluetooth Over Ear Headphones with Mic for Phone Calls, 30 Hours Battery Life, Quick Charge, AUX, Touch Control and Voice Control - BlacK...",
+        price : 12000,
+        image : P1,
+        rating : 4.1
+    }
+]
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Navbar.css'
+import logo from 'C:\Projects\e-commerce\src\Components\Assests\logo.png'
+import like from 'C:\Projects\e-commerce\src\Components\Assests\Like.png'
+import cart from 'C:\Projects\e-commerce\src\Components\Assests\Cart.png'
+import profile from 'C:\Projects\e-commerce\src\Components\Assests\Profile.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="nav_logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul className="nav_menu">
+        <li>Search</li>
+        <li>Home</li>
+        <li>Categories</li>
+        <li>Order</li>
+      </ul>
+      <div className="nav-login-cart">
+        <img src={like} alt="Like" />
+        <img src={cart} alt="Cart" />
+        <img src={profile} alt="Profile" />
+      </div>
+    </div>
   )
 }
 
