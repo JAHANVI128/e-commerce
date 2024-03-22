41a76967f40394804b621f3498c9f4b34747887d
acer <acer@PRO500AM4-M5>
2024-03-22 18:05:32 +0530

    link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import like from "../Assets/Like.png";
@@ -14,29 +14,40 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-logo">
+      <Link style={{textDecoration : 'none'}} to = '/'>
         <img src={logo} alt="Logo" />
+      </Link>
       </div>
       <ul className="nav-menu">
         <SearchBar />
 
         <li onClick={ () => { setmenu("home") }}> 
-          <Link to = '/'> Home </Link> {menu === "home" ? <hr /> : <></>}
+          <Link style={{textDecoration : 'none'}} to = '/'> Home </Link> {menu === "home" ? <hr /> : <></>}
         </li>
 
         <li onClick={() => { setmenu("categories") }}>
-        <Link to = '/category'> Categories </Link> {menu === "categories" ? <hr /> : <></>}
+          <Link style={{textDecoration : 'none'}} to = '/category'> Categories </Link> {menu === "categories" ? <hr /> : <></>}
         </li>
 
         <li onClick={() => { setmenu("orders") }}>
-        <Link to = '/orders'> Orders </Link> {menu === "orders" ? <hr /> : <></>}
+          <Link style={{textDecoration : 'none'}} to = '/orders'> Orders </Link> {menu === "orders" ? <hr /> : <></>}
           <div className="nav-order-count">1</div>
         </li>
       </ul>
       <div className="nav-login-cart">
-        <img className="like" src={like} alt="Like" />
-        <img className="cart" src={cart} alt="Cart" />
+        <Link style={{textDecoration : 'none'}}>
+          <img className="like" src={like} alt="Like" />
+        </Link>
+
+        <Link style={{textDecoration : 'none'}}>
+          <img className="cart" src={cart} alt="Cart" />
+        </Link>
+
         <div className="nav-cart-count">0</div>
-        <img className="profile" src={profile} alt="Profile" />
+
+        <Link style={{textDecoration : 'none'}}>
+          <img className="profile" src={profile} alt="Profile" />
+        </Link>
       </div>
     </div>
   );
